eccc1ede2bf5e8a7d06a1e4119f81b09a65c08b8
steel-a <stell4b@gmail.com>
2020-06-01 12:06:39 -0300

    Basic implementation finished

--- __init__.py
+++ __init__.py
@@ -6,14 +6,17 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
 class BrowserPy:
+    """
+    -> An abstract for selenium on an object.
+    ##### Note: To run this class on a Docker container, use one of these params on 'docker run' command: --shm-size=1G or -v /dev/shm:/dev/shm
+    """
     keys = Keys
     by = By
+    driver = None
 
-    def __init__(self, profile:str='chrome-headless'):
-        self.driver = None
+    def __init__(self, profile:str='chrome-headless-docker'):
 
-
-        if profile=='chrome-headless':
+        if profile=='chrome-headless-docker':
 
             from selenium.webdriver.chrome.options import Options
             chrome_options = Options()
@@ -23,7 +26,6 @@ class BrowserPy:
             chrome_options.add_argument("--no-sandbox") # linux only
             chrome_options.add_argument("--headless")
             # chrome_options.headless = True # also works
-            #chrome_options.add_argument("--disable-dev-shm-usage") # or in docker run command: --shm-size=1G or -v /dev/shm:/dev/shm
             self.driver = webdriver.Chrome(options=chrome_options)
 
         else:
@@ -31,122 +33,153 @@ class BrowserPy:
 
 
     def __del__(self):
-        if self.driver is None:
-            self.driver.quit()
+        if self.driver is not None:
+            self.driver.close()
 
 
     def open(self, url:str, assertText:str=None, assertAttemps:int=1, assertTime:float=1):
         """
         -> Loads a web page in the current browser session
         :param url: Url to be openned
-        :param assertText: After page loads, it should has this text
-        :return: True if page is load and have 'assertText' (case this parameter is used). False otherwise.
+        :param assertText: Text that must be in page
+        :param assertAtemps: How many tries it will be done
+        :param assertTime: Time (in secons) between tries
+        :return: True/False -> True if page is load and have 'assertText' (case this parameter is used). False otherwise.
         """
         self.driver.get(url)
         return self.assertText(assertText, assertAttemps, assertTime)
 
 
     def assertText(self, assertText:str, assertAttemps:int=1, assertTime:float=1) -> bool:
+        """
+        -> Search for a text inside actual page text
+        :param assertText: Text that must be in page
+        :param assertAtemps: How many tries it will be done
+        :param assertTime: Time (in secons) between tries
+        :return: True/False. True if assertText is in actual page text
+        """
         if assertText is None:
-            return True
+            return True #1
 
         for _ in range(assertAttemps):
             if assertText in self.getText():
-                return True
+                return True #2
             time.sleep(assertTime)
 
-        return True        
+        return False #3
 
 
     def getText(self, by:str='tag', name:str='html', regexPattern:str=None, regexGroup:int=0):
+        """
+        -> Get the text of an element
+        :param by: What type of element will be searched
+        :param name: Param to search the element
+        :param regexPattern: a regex expression to be searched inside the text of the element
+        :param regexGroup: wich regex group will be returnet
+        :return: Text found or '' otherwise
+        """        
         e = self.el(by, name)
         if e is None:
-            return ''
+            return '' #1
         
         if regexPattern is None:
-            return e.text
+            return e.text #2
 
         match = re.search(regexPattern, e.text) 
         if match:
             try:
-                return match.group(regexGroup)
+                return match.group(regexGroup) #3
             except:
-                pass
+                pass #->4
 
-        return ''            
+        return '' #4, 5
 
 
     def mapBy(self, by:str):
         if by=="id" or by==By.ID:
-            return By.ID
+            return By.ID #1,2
         elif by=="class" or by==By.CLASS_NAME:
-            return By.CLASS_NAME
+            return By.CLASS_NAME #3,4
         elif by=="css" or by==By.CSS_SELECTOR:
-            return By.CSS_SELECTOR
+            return By.CSS_SELECTOR #5,6
         elif by=="link" or by==By.LINK_TEXT:
-            return By.LINK_TEXT
+            return By.LINK_TEXT #7,8
         elif by=="name" or by==By.NAME:
-            return By.NAME
+            return By.NAME #9,10
         elif by=="partialLink" or by==By.PARTIAL_LINK_TEXT:
-            return By.PARTIAL_LINK_TEXT
+            return By.PARTIAL_LINK_TEXT #11,12
         elif by=="tag" or by==By.TAG_NAME:
-            return By.TAG_NAME
+            return By.TAG_NAME #13,14
         elif by=="xpath" or by==By.XPATH:
-            return By.XPATH
+            return By.XPATH #15,16
         else:
-            raise ValueError(f'By [{by}] could not be mapped.')
+            raise ValueError(f'By [{by}] could not be mapped.') #17
 
 
     def getElements(self, by:str, name:str):
-        elements = self.driver.find_elements(self.mapBy(by),name)
-        if len(elements)>0:
-            return elements
-        else:
-            return None
+        """
+        -> Gets all elements with type 'by' and 'name'
+        :return: List of elements or a empty list
+        """
+        return self.driver.find_elements(self.mapBy(by),name) #1
 
 
     def el(self, by:str, name:str, text:str=None, textExactMatch:bool=True):
+        """
+        -> Get an element described by params 'by' and 'name' with the (optional) text 'text'
+        :return: The element of type WebElement or None
+        """
         if(text is None):
             try:
                 el = self.driver.find_element(self.mapBy(by),name)
             except:
-                return None
-            return el
+                return None #1
+            return el #2
         else:
             elements = self.getElements(by, name)
-            if elements is None:
-                return None
+            if len(elements)==0:
+                return None #3
             for e in elements:
                 if(textExactMatch==True and text==e.text) or \
                     (textExactMatch==False and text in e.text):
-                    return e
-        return None
+                    return e #4
+        return None #5
 
 
     def click(self, el:WebElement, assertText:str=None, assertAttemps:int=3, assertTime:float=1) -> bool:
+        """
+        -> Click on element 'el' and optionally assert for a text on page after the click
+        :return: True/False. True if click and then page has the text, False otherwise
+        """
         if el is None:
-            return False
+            return False #1
 
         try:
             el.click()
         except:
-            return False
+            return False #2
 
-        return self.assertText(assertText, assertAttemps, assertTime)
+        return self.assertText(assertText, assertAttemps, assertTime) #3
 
 
     def sendKeys(self, el:WebElement, keys, assertText:str=None, assertAttemps:int=3, assertTime:float=1) -> bool:
+        """
+        -> Send keys to an element and optionally assert for a text on page after the send
+        :return: True/False. True if send keys and then page has the text, False otherwise
+        """
         if el is None:
-            return False
+            return False #1
 
         try:
-            if isinstance(keys, str):
+            if isinstance(keys, str): #->2
                 el.send_keys(keys)
-            elif isinstance(keys,tuple):
+            elif isinstance(keys,tuple): #->3
                 for k in keys:
                     el.send_keys(k)
-        except: # Exception as inst:
-            return False
+            else:
+                return False #4
+        except:
+            return False #5
 
-        return self.assertText(assertText, assertAttemps, assertTime)
+        return self.assertText(assertText, assertAttemps, assertTime) #2 and 3
 
--- tests.py
+++ tests.py
@@ -3,16 +3,68 @@ from browserpy import BrowserPy
 b = BrowserPy()
 
 # Open github.com and Assert that 'Why GitHub?' is in openned page
-b.open('http://www.github.com','Why GitHub?')
+assert b.open('http://www.github.com','Why GitHub?')
 
-# Click link 'Marketplace'
-if not b.click(b.el(b.by.LINK_TEXT,'Marketplace'),"Extend GitHub"):
-    print("Error clicking in the link 'Marketplace'")
+# function assertText
+assert b.assertText(None) #1
+assert b.assertText('Why GitHub?') #2
+assert b.assertText('Why GitHub?',5) #2
+assert b.assertText('Why GitHub?',2,5) #2
+assert b.assertText('Why GitHub?',0,0) == False #3
+assert b.assertText('Loren ipsun') == False #3
 
-# Send Keys 'python'and then 'ENTER' to selected element
-if not b.sendKeys(b.el(b.by.CLASS_NAME,'header-search-input'),('python',b.keys.ENTER),'repository results'):
-    print("Error senging keys to element'")
+# function mapBy
+assert b.mapBy(b.by.ID) == b.by.ID #1
+assert b.mapBy('id') == b.by.ID #2
+assert b.mapBy(b.by.CLASS_NAME) == b.by.CLASS_NAME #3
+assert b.mapBy('class') == b.by.CLASS_NAME #4
+assert b.mapBy(b.by.CSS_SELECTOR) == b.by.CSS_SELECTOR #5
+assert b.mapBy('css') == b.by.CSS_SELECTOR #6
+assert b.mapBy(b.by.LINK_TEXT) == b.by.LINK_TEXT #7
+assert b.mapBy('link') == b.by.LINK_TEXT #8
+assert b.mapBy(b.by.NAME) == b.by.NAME #9
+assert b.mapBy('name') == b.by.NAME #10
+assert b.mapBy(b.by.PARTIAL_LINK_TEXT) == b.by.PARTIAL_LINK_TEXT #11
+assert b.mapBy('partialLink') == b.by.PARTIAL_LINK_TEXT #12
+assert b.mapBy(b.by.TAG_NAME) == b.by.TAG_NAME #13
+assert b.mapBy('tag') == b.by.TAG_NAME #14
+assert b.mapBy(b.by.XPATH) == b.by.XPATH #15
+assert b.mapBy('xpath') == b.by.XPATH #16
+try:
+    b.mapBy('aaaa')
+except ValueError:
+    assert True #17
 
-# Click on selected element and check for 'repository results' in page
-print(b.getText(regexPattern='([0-9,]*) repository results'))
-print(b.getText(regexPattern='([0-9,]*) repository results', regexGroup=1))
+# function getElements
+assert len(b.getElements('tag','a')) > 0 #1
+assert len(b.getElements('tag','aaaaaa')) == 0 #1
+
+# function el
+assert b.el('aaa','a') == None #1
+assert b.el('tag','a').text != None #2
+assert b.el('tag','aaaaa','aaaaa') == None #3
+assert b.el('tag','a','Marketplace').text == 'Marketplace' #4
+assert b.el('tag','a','aaaaa') == None #5
+
+# function click - I could not test #2 return
+assert b.click(b.el('link','aaaaa')) == False #1
+assert b.click(b.el(b.by.LINK_TEXT,'Marketplace'),"Loren ipsun") == False #3
+assert b.click(b.el(b.by.LINK_TEXT,'Marketplace'),"Extend GitHub") #3
+
+# function sendKeys - I could not test #5 return
+assert b.sendKeys(b.el('link','aaaaa'),'keys') == False #1
+assert b.sendKeys(b.el(b.by.CLASS_NAME,'header-search-input'),'python') #2
+assert b.sendKeys(b.el(b.by.CLASS_NAME,'header-search-input'),b.keys.ENTER,'repository results') #2
+assert b.sendKeys(b.el(b.by.CLASS_NAME,'header-search-input'),('python',b.keys.ENTER),'repository results') #3
+assert b.sendKeys(b.el(b.by.CLASS_NAME,'header-search-input'),('python',b.keys.ENTER),'Loren ipsun') == False #3
+assert b.sendKeys(b.el(b.by.CLASS_NAME,'header-search-input'),list(range(5))) == False #4
+
+# function getText
+assert b.getText('tag','aaaaaaa') == '' #1
+assert b.getText('tag','html') != '' #2
+assert 'repository results' in b.getText(regexPattern='([0-9,]*) repository results') #3
+assert 'repository results' in b.getText(regexPattern='[0-9,]* (repository results)', regexGroup=1) #3
+assert 'repository results' in b.getText(b.by.TAG_NAME, 'html', '[0-9,]* (repository results)', 1) #3
+assert b.getText(b.by.TAG_NAME, 'html', '[0-9,]* (repository results)', 5) == '' #4
+assert b.getText(b.by.TAG_NAME, 'html', '[0-9,]* (aaaaaaaaaa)', 1) == '' #5
+assert b.getText(b.by.TAG_NAME, 'html', '[0-9,]* (aaaaaaaaaa)') == '' #5
